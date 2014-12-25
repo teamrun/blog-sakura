7aecde5680930969899405e6e08ce00e2c161027
teamrun <chenllos@163.com>
2014-12-25 16:09:10 +0800

    config 启动时默认创建photo的容器文件夹

--- config.js
+++ config.js
@@ -1,17 +1,38 @@
 var path = require('path');
 var fse = require('fs-extra');
 
+var HOME = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+var photoLib = path.resolve(HOME, 'picture/blog-sakura');
+
 var conf = {
     db: {
         url: 'localhost/blog-sakura'
     },
-    uploadTmp: path.resolve('./.uploadTmp')
+    uploadTmp: path.resolve('./.uploadTmp'),
+    photo: {
+        lib: photoLib,
+        thumb: path.join(photoLib, 'thumb'),
+        thumb_2x: path.join(photoLib, 'thumb_2x')
+    }
 };
 
-var needed = ['uploadTmp'];
+var needed = ['uploadTmp', 'photo.lib', 'photo.thumb', 'photo.thumb_2x'];
+
+
+function objAccess(obj, key){
+    if(key.indexOf('.') < 0){
+        return obj[key];
+    }
+    else{
+        var arr = key.split('.');
+        var t = arr[0];
+        var leftKey = arr.slice(1).join('.');
+        return objAccess(obj[t], leftKey );
+    }
+}
 
 needed.forEach(function(p){
-    fse.mkdirpSync(conf[p]);
+    fse.mkdirpSync( objAccess(conf,p) );
 });
 
 module.exports = conf;
